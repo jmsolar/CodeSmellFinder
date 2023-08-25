ece021e7a826faf52825e550b79a30630b103e20
Jonatan Matias Solar <matias.solar@outlook.com>
2023-08-25 16:26:31 -0300

    Resource service implementation to receive message by language

--- SmellFinderTool/Core/Services/Implementations/ResourceService.cs
+++ SmellFinderTool/Core/Services/Implementations/ResourceService.cs
@@ -1,5 +1,7 @@
+using System;
 using System.Collections.Generic;
 using System.Globalization;
+using System.Linq;
 using System.Reflection;
 using System.Resources;
 using SmellFinderTool.Core.Services.Interfaces;
@@ -9,29 +11,49 @@ namespace SmellFinderTool.Core.Services.Implementations
     public class ResourceService : IResourceService
     {
         #region Fields
+        private CultureInfo CultureInfo;
         private ResourceManager _resourceManager;
-        public CultureInfo CultureInfo;
+        private readonly string defaultISOCode = "en-us"; 
+        private readonly HashSet<string> ValidLanguageCodes = new HashSet<string>(CultureInfo.GetCultures(CultureTypes.AllCultures).Select(culture => culture.Name.ToLower()));
         #endregion
 
         #region Constructors
         public ResourceService()
         {
-            Assembly assembly = Assembly.GetExecutingAssembly();
             string baseResourceName = "SmellFinderTool.Resources.Strings";
-            _resourceManager = new ResourceManager(baseResourceName, assembly);
-            CultureInfo = new CultureInfo("en-US");
+            _resourceManager = new ResourceManager(baseResourceName, Assembly.GetExecutingAssembly());
         }
         #endregion
 
         #region Methods
-        public string GetStringResource(string key, List<string> valuesToFormat = null, string cultureName = "en-US")
+        public void SetLanguage(string ISOCode) {
+            try
+            {
+                ISOCode = ISOCode?.ToLower();
+                string lang = IsLanguageValid(ISOCode) ? ISOCode : defaultISOCode;
+                CultureInfo = new CultureInfo(lang);   
+            }
+            catch (Exception)
+            {
+                CultureInfo = new CultureInfo(defaultISOCode);
+            }
+        }
+
+        private bool IsLanguageValid(string ISOCode) => ValidLanguageCodes.Contains(ISOCode);
+
+        public string GetStringResource(string key, List<string> valuesToFormat = null)
         {
-            if (!string.IsNullOrEmpty(cultureName)) CultureInfo = new CultureInfo(cultureName);
+            try
+            {
+                string localizedText = _resourceManager.GetString(key, CultureInfo);
+                localizedText = valuesToFormat == null ? localizedText : string.Format(localizedText, valuesToFormat.ToArray());
 
-            string localizedText = _resourceManager.GetString(key, CultureInfo);
-            localizedText = string.Format(localizedText, valuesToFormat.ToArray());
-
-            return localizedText;
+                return localizedText;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
         }
         #endregion
     }
--- SmellFinderTool/Core/Services/Interfaces/IResourceService.cs
+++ SmellFinderTool/Core/Services/Interfaces/IResourceService.cs
@@ -4,6 +4,7 @@ namespace SmellFinderTool.Core.Services.Interfaces
 {
     public interface IResourceService
     {
-        string GetStringResource(string key, List<string> valuesToFormat = null, string cultureName = "en-US");
+        void SetLanguage(string ISOCode);
+        string GetStringResource(string key, List<string> valuesToFormat = null);
     }
 }
