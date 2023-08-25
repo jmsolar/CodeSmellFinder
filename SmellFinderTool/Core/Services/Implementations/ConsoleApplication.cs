ab3c2c81776b1f471c158fac6e42fff4da47e508
Jonatan Matias Solar <matias.solar@outlook.com>
2023-08-25 16:28:55 -0300

    Refactors and improvements general

--- SmellFinderTool/Core/Services/Implementations/ConsoleApplication.cs
+++ SmellFinderTool/Core/Services/Implementations/ConsoleApplication.cs
@@ -17,25 +17,28 @@ namespace SmellFinderTool.Core.Services.Implementations
         private readonly IFileSystemManagerService _fileSystemManager;
         private readonly AssemblyModel _assembly;
         private IReportWriterStrategy _reportWriter;
-        private ReportSettings _config;
         private readonly IResourceService _textResource;
+        private readonly LanguageSettings _languageSettings;
+        private string _message;
+        private string _directoryName;
+        private List<string> _filesToProcess;
+        private List<string> _smellsToAnalyze;
+        private string _outputExtension;
+        private string _filenameOutput;
+        private string _reprocess;
+        private List<SmellReportedModel> _listOfSmells;
         #endregion
 
         #region Constructors
-        public ConsoleApplication(
-            IDisplayerService displayer,
-            IAnalizerService analizer,
-            IFileSystemManagerService fileSystemManager,
-            ReportSettings config,
-            IResourceService resourceService
+        public ConsoleApplication(IDisplayerService displayer, IAnalizerService analizer, IFileSystemManagerService fileSystemManager,
+            LanguageSettings languageSettings, IResourceService resourceService
         )
         {
             _displayer = displayer;
             _analizer = analizer;
             _fileSystemManager = fileSystemManager;
-            _config = config;
             _textResource = resourceService;
-
+            _languageSettings = languageSettings;
             _assembly = new AssemblyModel(
                     name: Assembly.GetExecutingAssembly().GetName().Name,
                     version: Assembly.GetExecutingAssembly().GetName().Version
@@ -46,9 +49,8 @@ namespace SmellFinderTool.Core.Services.Implementations
         #region Methods
         public async Task Run()
         {
+            Init();
             bool follow = true;
-            string directoryName = string.Empty;
-            string message = string.Empty;
 
             while (follow)
             {
@@ -56,50 +58,102 @@ namespace SmellFinderTool.Core.Services.Implementations
                 {
                     do
                     {
-                        _displayer.Init(_assembly);
-                        message = _textResource.GetStringResource("askDirectoryName", new List<string> { {MessageFormatterModel.GetFormattedText("springgreen4", "code smells")} });
-                        directoryName = _displayer.ShowAskMessage($"{message}");
+                        ShowAppInfo();
+                        ShowDirectorySelector();
+                        _filesToProcess = await _fileSystemManager.FilesToProcess(_directoryName);
+                        _smellsToAnalyze = ShowCodeSmellsSelector();
 
-                        var filesToProcess = await _fileSystemManager.GetFilesToProcess(directoryName);
-                        var optionsSelected = _displayer.ShowMenu(_analizer.GetOptions());
-
-                        if (optionsSelected.Any())
+                        if (_smellsToAnalyze.Any())
                         {
-                            _displayer.ShowDirectorySelected(directoryName);
-                            var outputExtension = _displayer.ShowOutputExtension(_config);
-                            _displayer.ShowCounterOfFiles(filesToProcess.Count);
+                            ShowReportTypeSelector();
+                            ShowTotalFilesToProcess();
 
-                            var filenameOutput = _fileSystemManager.GetFileNameOutput(directoryName, outputExtension);
+                            if (_filesToProcess.Count > 0) {
+                                _filenameOutput = _fileSystemManager.FilenameOutput(_directoryName, _outputExtension);
 
-                            var action = new Action(() => _analizer.SearchSmellsOnDirectory(filesToProcess, optionsSelected));
-                            var res = _displayer.ShowProgress(() =>
-                            {
-                                return _analizer.SearchSmellsOnDirectory(filesToProcess, optionsSelected);
-                            });
-
-                            _reportWriter = SetStrategy(outputExtension);
-                            _reportWriter.WriteReport(filenameOutput, res);
-                            _displayer.ShowEndOfProcess(filenameOutput);
+                                SearchCodeSmells();
+                                WriteReport();
+                                ShowProcessResult();
+                            }
                         }
 
-                        follow = _displayer.ShowConfirmMessage(_textResource.GetStringResource("reprocess"));
+                        follow = _displayer.ShowConfirmMessage(_reprocess);
 
                         if (follow) _displayer.ClearScreen();
                     } while (follow);
                 }
                 catch (Exception ex)
                 {
-                    _displayer.ShowSimpleMessage(ex.Message);
-                    follow = _displayer.ShowConfirmMessage("Want you search others smells?");
+                    _displayer.ShowException(ex);
+                    follow = _displayer.ShowConfirmMessage(_reprocess);
                 }
             }
         }
         #endregion
 
         #region Private methods
-        private IReportWriterStrategy SetStrategy(string outputExtension)
+        private void Init()
         {
-            switch (outputExtension.ToLower())
+            try
+            {
+                _textResource.SetLanguage(_languageSettings.ISOCode);
+                _reprocess = _textResource.GetStringResource("reprocess");
+                _displayer.Init();
+            }
+            catch (Exception ex)
+            {
+                _displayer.ShowException(ex);
+            }
+        }
+
+        private void ShowAppInfo()
+        {
+            _displayer.ClearScreen();
+            _message = string.Join(
+                            " ",
+                            $"{MessageFormatterModel.GetFormattedText("green bold", _assembly.Name)}",
+                            $"{MessageFormatterModel.GetFormattedText("white bold", _assembly.Version.ToString())}"
+                        );
+            _displayer.ShowAppInfo(_message);
+        }
+
+        private void ShowDirectorySelector()
+        {
+            _message = _textResource.GetStringResource("askDirectoryName", new List<string> { MessageFormatterModel.GetFormattedText("springgreen4", "code smells") });
+            _directoryName = _displayer.ShowAskMessage(_message);
+        }
+
+        private List<string> ShowCodeSmellsSelector() => _displayer.ShowMenu(_analizer.GetOptions());
+
+        private void ShowReportTypeSelector() => _outputExtension = _displayer.ShowOutputExtension();
+
+        private void ShowTotalFilesToProcess()
+        {
+            _message = _textResource.GetStringResource("notFoundFile");
+            if (_filesToProcess.Count > 0)
+                _displayer.ShowStatus(TaskStatusModel.INFO, _textResource.GetStringResource("numberOfFile").Replace("{1}", _filesToProcess.Count.ToString())); 
+            else 
+                _displayer.ShowStatus(TaskStatusModel.WARN, _textResource.GetStringResource("notFoundFile"));
+        }
+
+        private void SearchCodeSmells() => _listOfSmells = _displayer.ShowProgress(() => _analizer.SearchSmellsOnDirectory(_filesToProcess, _smellsToAnalyze));
+        private void WriteReport()
+        {
+            _reportWriter = SetStrategy();
+            _reportWriter.WriteReport(_filenameOutput, _listOfSmells);
+        }
+
+        private void ShowProcessResult()
+        {
+            string filenameOutput = MessageFormatterModel.GetFormattedText("italic", _filenameOutput);
+            _message = _textResource.GetStringResource("okResult").Replace("{1}", filenameOutput);
+            _displayer.ShowStatus(TaskStatusModel.SUCCESS, _message);
+        }
+
+        //TODO mejorar la forma en que evaluo a que estrategia debe asignarsele la responsabilidad de generar el reporte
+        private IReportWriterStrategy SetStrategy()
+        {
+            switch (_outputExtension.ToLower())
             {
                 case "json":
                     return new JSONReportWriterStrategy();
