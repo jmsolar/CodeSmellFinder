500e8258ccbe6c40e0317a99e0cadd9ff3fa68a6
Jonatan Matias Solar <matias.solar@outlook.com>
2023-08-25 16:28:20 -0300

    Improvement general skills

--- SmellFinderTool/Core/Services/Implementations/DisplayerService.cs
+++ SmellFinderTool/Core/Services/Implementations/DisplayerService.cs
@@ -9,73 +9,98 @@ namespace SmellFinderTool.Core.Services.Implementations
 {
     public class DisplayerService : IDisplayerService
     {
-        #region Methods
-        public void Init(AssemblyModel assembly)
+        #region Fields
+        private readonly IResourceService _textResource;
+        private ReportSettings _reportSettings;
+        private readonly ExceptionSettings _exceptionSettings = new ExceptionSettings
         {
-            ClearScreen();
-            AnsiConsole.Write(
-                new Rule(
-                        string.Join(
-                            " ",
-                            $"{MessageFormatterModel.GetFormattedText("green bold", assembly.Name)}",
-                            $"{MessageFormatterModel.GetFormattedText("white bold", assembly.Version.ToString())}"
-                        )
-                )
-            );
+            Format = ExceptionFormats.ShortenEverything,
+            Style = new ExceptionStyle
+            {
+                Exception = new Style().Foreground(Color.Red),
+                Message = new Style().Foreground(Color.White),
+                NonEmphasized = new Style().Foreground(Color.White),
+                Parenthesis = new Style().Foreground(Color.White),
+                Method = new Style().Foreground(Color.Red),
+                ParameterName = new Style().Foreground(Color.White),
+                ParameterType = new Style().Foreground(Color.Red),
+                Path = new Style().Foreground(Color.Red),
+                LineNumber = new Style().Foreground(Color.White),
+            }
+        };
+        private readonly int _pageSizeMenu = 4;
+
+        /* MESSAGES */
+        private string _progressMsg;
+        private string _titleSmellSelectorMsg;
+        private string _spaceText;
+        private string _enterText;
+        private string _instructionsText;
+        private string _moreChoiceMsg;
+        private string _all;
+        private string _titleReportSelectorMsg;
+        private string _outputFileExtensionMsg;
+        private char _yes;
+        private char _no;
+        private Dictionary<TaskStatusModel, List<string>> _statusMsg;
+        #endregion
+
+        #region Constructors
+        public DisplayerService(IResourceService resourceService, ReportSettings reportSettings)
+        {
+            _textResource = resourceService;
+            _reportSettings = reportSettings;
         }
+        #endregion
+
+        #region Methods
+        public void Init()
+        {
+            _progressMsg = MessageFormatterModel.GetFormattedText("yellow", _textResource.GetStringResource("progress"));
+            _titleSmellSelectorMsg = _textResource.GetStringResource("askSmellToValidate", new List<string> { MessageFormatterModel.GetFormattedText("springgreen4", "code smells") });
+            _spaceText = MessageFormatterModel.GetFormattedText("blue", _textResource.GetStringResource("selectorKey"));
+            _enterText = MessageFormatterModel.GetFormattedText("springgreen4", _textResource.GetStringResource("continueKey"));
+            _instructionsText = MessageFormatterModel.GetFormattedText("gray", _textResource.GetStringResource("smellsInstructions", new List<string> { _spaceText, _enterText }));
+            _moreChoiceMsg = MessageFormatterModel.GetFormattedText("gray", _textResource.GetStringResource("menuChoiceText"));
+            _all = _textResource.GetStringResource("all");
+
+            _titleReportSelectorMsg = _textResource.GetStringResource("chooseExtensionFile");
+            _outputFileExtensionMsg = MessageFormatterModel.GetFormattedText("deepskyblue3_1", _textResource.GetStringResource("outputFileExtensionSelected"));
+        
+            _statusMsg = new Dictionary<TaskStatusModel, List<string>>
+            {
+                { TaskStatusModel.SUCCESS, new List<string> { MessageFormatterModel.GetFormattedText("white on green bold", _textResource.GetStringResource("success")), "green" } },
+                { TaskStatusModel.INFO, new List<string> { MessageFormatterModel.GetFormattedText("white on blue bold", _textResource.GetStringResource("info")), "blue" } },
+                { TaskStatusModel.WARN, new List<string> { MessageFormatterModel.GetFormattedText("white on yellow bold", _textResource.GetStringResource("warn")), "yellow" } },
+                { TaskStatusModel.FAIL, new List<string> { MessageFormatterModel.GetFormattedText("white on red bold", _textResource.GetStringResource("error")), "red" } }
+            };
+
+            _yes = char.Parse(_textResource.GetStringResource("yes"));
+            _no = char.Parse(_textResource.GetStringResource("no"));
+        }
+
+        public void ShowStatus(TaskStatusModel taskStatus, string message)
+        {
+            if (_statusMsg.TryGetValue(taskStatus, out List<string> statusMessage)) {
+                message = MessageFormatterModel.GetFormattedText(statusMessage[1], message);
+                ShowSimpleMessage($"\n{message.Replace("{0}", statusMessage[0])}");
+            }
+        }
+
+        public void ShowAppInfo(string message) => AnsiConsole.Write(new Rule(message));
 
         public string ShowAskMessage(string message) => AnsiConsole.Ask<string>(message);
 
-        public void ClearScreen() => AnsiConsole.Clear();
-
-        public void ShowDirectorySelected(string directoryName) =>
-            AnsiConsole.MarkupLine(
-                string.Join(
-                    " ",
-                    $"{MessageFormatterModel.GetFormattedText("deepskyblue3_1", "\nThe directories and subdirectories of")}",
-                    $"{MessageFormatterModel.GetFormattedText("gold3_1 italic", directoryName)}",
-                    $"{MessageFormatterModel.GetFormattedText("deepskyblue3_1", "would be analyzed")}"
-                )
-            );
-
-        public List<string> ShowMenu(Dictionary<string, string> options)
-        {
-            List<string> selected = AnsiConsole.Prompt(
-                new MultiSelectionPrompt<string>()
-                    .Title($"What {MessageFormatterModel.GetFormattedText("springgreen4", "code smell")} do you need to validate?")
-                    .NotRequired()
-                    .PageSize(5)
-                    .MoreChoicesText($"{MessageFormatterModel.GetFormattedText("grey", "(Move up and down to reveal more options)")}")
-                    .InstructionsText(
-                        string.Join(
-                            " ",
-                            $"{MessageFormatterModel.GetFormattedText("grey", "(Press")}",
-                            $"{MessageFormatterModel.GetFormattedText("blue", "<space>")}",
-                            $"{MessageFormatterModel.GetFormattedText("grey", "to toggle a code smell,")}",
-                            $"{MessageFormatterModel.GetFormattedText("green", "<enter>")}",
-                            $"{MessageFormatterModel.GetFormattedText("gray", "to accept and begin process)")}"
-                        )
-                    )
-                    .AddChoiceGroup("All", options.Values)
-            );
-
-            if (selected.Any())
-            {
-                ShowOptionsSelected(selected);
-
-                return ConvertSmellToVisitor(options, selected);
-            }
-
-            AnsiConsole.MarkupLine(
-                string.Join(
-                    " ",
-                    $"{MessageFormatterModel.GetFormattedText("white on yellow", "\n WARN ")}",
-                    $"{MessageFormatterModel.GetFormattedText("yellow", "Should be select a option to move on")}"
-                )
-            );
-
-            return selected;
+        public bool ShowConfirmMessage(string message) {
+            var prompt = new ConfirmationPrompt(message);
+            prompt.Yes(char.Parse(_textResource.GetStringResource("yes")));
+            prompt.No(char.Parse(_textResource.GetStringResource("no")));
+            return prompt.Show(AnsiConsole.Console);
         }
+
+        public void ShowSimpleMessage(string message) => AnsiConsole.MarkupLine(message);
+
+        public void ClearScreen() => AnsiConsole.Clear();
 
         public List<SmellReportedModel> ShowProgress(Func<List<SmellReportedModel>> task)
         {
@@ -84,7 +109,7 @@ namespace SmellFinderTool.Core.Services.Implementations
             AnsiConsole
                 .Status()
                 .AutoRefresh(true)
-                .Start($"{MessageFormatterModel.GetFormattedText("yellow", "Searching bad smells...")}", ctx =>
+                .Start(_progressMsg, ctx =>
                 {
                     result = task.Invoke();
                 });
@@ -92,68 +117,59 @@ namespace SmellFinderTool.Core.Services.Implementations
             return result;
         }
 
-        public void ShowCounterOfFiles(int numberOfFiles)
+        public List<string> ShowMenu(Dictionary<string, string> options)
         {
-            var message = "Not found files to be processed";
-
-            if (numberOfFiles > 0) message = $"There are {numberOfFiles} files to be processed";
-
-            AnsiConsole.MarkupLine($"\n{MessageFormatterModel.GetFormattedText("blue on white bold", message)}");
-        }
-
-        public void ShowEndOfProcess(string fileNameOutput) =>
-            AnsiConsole.MarkupLine(
-                string.Join(
-                    " ",
-                    $"{MessageFormatterModel.GetFormattedText("white on springgreen4", "\n OK ")}",
-                    $"{MessageFormatterModel.GetFormattedText("green", "Review the result in the output file")}",
-                    $"{MessageFormatterModel.GetFormattedText("white italic", fileNameOutput)}"
-                )
+            List<string> selected = AnsiConsole.Prompt(
+                new MultiSelectionPrompt<string>()
+                    .Title(_titleSmellSelectorMsg)
+                    .Required()
+                    .PageSize(_pageSizeMenu)
+                    .MoreChoicesText(_moreChoiceMsg)
+                    .InstructionsText(_instructionsText)
+                    .AddChoiceGroup(_all, options.Values)
             );
 
-        public string ShowOutputExtension(ReportSettings config)
+            if (selected.Any())
+            {
+                ShowOptionsSelected(selected);
+                return ConvertSmellToVisitor(options, selected);
+            }
+
+            ShowStatus(TaskStatusModel.WARN, _textResource.GetStringResource("selectOption"));
+
+            return selected;
+        }
+
+        public string ShowOutputExtension()
         {
             var optionSelected = AnsiConsole.Prompt(
                 new SelectionPrompt<string>()
-                    .Title($"\nChoose the {MessageFormatterModel.GetFormattedText("springgreen4", "format of the report")} you want to generate")
-                    .PageSize(5)
-                    .MoreChoicesText($"{MessageFormatterModel.GetFormattedText("grey", "(Move up and down to reveal more options)")}")
-                    .AddChoices(config.FileExtension.Select(x => x.Description).ToList())
+                    .Title(_titleReportSelectorMsg)
+                    .PageSize(_pageSizeMenu)
+                    .MoreChoicesText(_moreChoiceMsg)
+                    .AddChoices(_reportSettings.FileExtension.Select(x => x.Description).ToList())
             );
 
-            var fileExtensionSelected = ConvertDescriptionToFileExtension(config.FileExtension, optionSelected);
-
-            AnsiConsole.Markup(
-                string.Join(
-                    " ",
-                    $"{MessageFormatterModel.GetFormattedText("deepskyblue3_1", "You selected")}",
-                    $"{MessageFormatterModel.GetFormattedText("gold3_1 italic", optionSelected)}",
-                    $"{MessageFormatterModel.GetFormattedText("deepskyblue3_1", "as output format to report")}"
-                )
-            );
+            var fileExtensionSelected = ConvertDescriptionToFileExtension(_reportSettings.FileExtension, optionSelected);
+            ShowSimpleMessage(string.Format(_outputFileExtensionMsg, MessageFormatterModel.GetFormattedText("gold3_1 italic", optionSelected)));
 
             return fileExtensionSelected;
         }
 
-        public bool ShowConfirmMessage(string message)
-        {
-            return AnsiConsole.Confirm(message);
-        }
-
-        public void ShowSimpleMessage(string message) {
-            AnsiConsole.MarkupLine(message);
-        }
+        public void ShowException(Exception ex) => AnsiConsole.WriteException(ex, _exceptionSettings);
         #endregion
 
         #region  Private methods
         private void ShowOptionsSelected(List<string> optionsSelected)
         {
-            AnsiConsole.MarkupLine($"{MessageFormatterModel.GetFormattedText("deepskyblue3_1", "You selected the following options")}");
+            ShowSimpleMessage(MessageFormatterModel.GetFormattedText("deepskyblue3_1", "\n" + _textResource.GetStringResource("optionsSelected")));
 
             foreach (var option in optionsSelected)
             {
-                AnsiConsole.MarkupLine($"* {MessageFormatterModel.GetFormattedText("gold3_1 italic", option)}");
+                ShowSimpleMessage($"* {MessageFormatterModel.GetFormattedText("gold3_1 italic", option)}");
             }
+
+            ShowSimpleMessage("");
         }
 
         private List<string> ConvertSmellToVisitor(Dictionary<string, string> options, List<string> selected) =>
--- SmellFinderTool/Core/Services/Interfaces/IDisplayerService.cs
+++ SmellFinderTool/Core/Services/Interfaces/IDisplayerService.cs
@@ -6,16 +6,16 @@ namespace SmellFinderTool.Core.Services.Interfaces
 {
     public interface IDisplayerService
     {
-         void Init(AssemblyModel assembly);
-         string ShowAskMessage(string message);
-         void ShowDirectorySelected(string directoryName);
-         void ClearScreen();
-         List<string> ShowMenu(Dictionary<string, string> options);
-         List<SmellReportedModel> ShowProgress(Func<List<SmellReportedModel>> task);
-         void ShowCounterOfFiles(int numberOfFiles);
-         void ShowEndOfProcess(string fileNameOutput);
-         string ShowOutputExtension(ReportSettings config);
-         bool ShowConfirmMessage(string message);
-         void ShowSimpleMessage(string message);
+        void Init();
+        void ShowAppInfo(string message);
+        string ShowAskMessage(string message);
+        void ClearScreen();
+        List<string> ShowMenu(Dictionary<string, string> options);
+        List<SmellReportedModel> ShowProgress(Func<List<SmellReportedModel>> task);
+        string ShowOutputExtension();
+        bool ShowConfirmMessage(string message);
+        void ShowSimpleMessage(string message);
+        void ShowException(Exception ex);
+        void ShowStatus(TaskStatusModel taskStatus, string message);
     }
 }
